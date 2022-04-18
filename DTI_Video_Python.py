b6232eebb9012605f8c3d3a9805014056951abbb
sihui3466 <103938218+sihui3466@users.noreply.github.com>
2022-04-18 20:15:21 +0800

    Update DTI_Video_Python.py

--- DTI_Video_Python.py
+++ DTI_Video_Python.py
@@ -3,29 +3,29 @@ import vlc
 import time
 
 
-media_player=vlc.MediaPlayer()
-movie1 = vlc.Media("Bike_Riding.mp4")
+media_player=vlc.MediaPlayer() 
+movie1 = vlc.Media("Bike_Riding.mp4") #play cycling video
 media_player.set_media(movie1)
 
-ser=serial.Serial("COM5", timeout=1)
+ser=serial.Serial("COM5", timeout=1) #set up connection between arduino and python
 
 while True:
-    command = ser.read()
+    command = ser.read() #read data from arduino
     if command:
         # flush serial for unprocessed data
         ser.flushInput()
         print("new command:", command)
-        decode=command.decode("utf-8")
+        decode=command.decode("utf-8") #convert data in arduino from bytes to integer
         print("decoded value: ",decode)
 ##        print("decoded command: ", decode)
-        if str(decode) == '1':
+        if str(decode) == '1': #if user is cycling (1) play the video
             print("Playing movie")
-            media_player.play()
-            time.sleep(0.5)
-        elif str(decode)=='0':
+            media_player.play() #play the video
+            time.sleep(0.5) #delay 0.5sec
+        elif str(decode)=='0': #if user is not cycling (0) pause the video
             print("pause")
-            media_player.set_pause(3)
-            time.sleep(0.5)
+            media_player.set_pause(3) #pause for 3 sec
+            time.sleep(0.5) #delay 0.5sec
 
             
 
